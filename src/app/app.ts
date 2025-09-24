143e10c684817c14eacfc4c4425ec1f0c87047e8
Sangam Verma <sangam.verma@biz2x.com>
2025-09-24 16:16:38 +0530

    toggle menu fixed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule,NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
